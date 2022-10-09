c1f2b52bcc6b8a58a156e832d6d53749d976f32a
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2022-10-09 16:52:30 +0530

    fix(popup): minor bugs

--- source/Popup/components/PopupButtons/PopupButtons.tsx
+++ source/Popup/components/PopupButtons/PopupButtons.tsx
@@ -1,91 +1,85 @@
-import classnames from 'classnames';
-import React, {useContext, useState, useEffect} from 'react';
-import {browser} from 'webextension-polyfill-ts';
-import {getIcon} from '../../../utils/geticon';
-import {ContextProvider} from '../../context/context';
-import {Shortcut, Space} from '../../reducers/types';
-import styles from './PopupButtons.module.scss';
+import classnames from 'classnames'
+import React, { useContext, useState, useEffect } from 'react'
+import { browser } from 'webextension-polyfill-ts'
+import { getIcon } from '../../../utils/geticon'
+import { ContextProvider } from '../../context/context'
+import { Shortcut, Space } from '../../reducers/types'
+import styles from './PopupButtons.module.scss'
 
 const PopupButtons: React.FC = () => {
-  const {state} = useContext(ContextProvider);
-  const [shortcutList, setshortcutList] = useState([]);
-  const [spaceList, setSpaceList] = useState([]);
-  const [categoryList, setCategoryList] = useState<string[]>();
-  useEffect(() => {
-    browser.storage.local.get().then((res) => {
-      setshortcutList(res.shortcutList);
-      setSpaceList(res.spaceList);
-    });
-  }, []);
+	const { state } = useContext(ContextProvider)
+	const [shortcutList, setshortcutList] = useState([])
+	const [spaceList, setSpaceList] = useState([])
+	const [, setCategoryList] = useState<string[]>()
+	useEffect(() => {
+		browser.storage.local.get().then((res) => {
+			setshortcutList(res.shortcutList)
+			setSpaceList(res.spaceList)
+		})
+	}, [])
+	console.log(state.isSubmitDisabled)
+	useEffect(() => {
+		browser.storage.local.get().then((res) => {
+			if (state.activeTab === 'shortcut') {
+				setCategoryList(res.shortcutList.map((item: Shortcut) => item.category))
+			} else if (state.activeTab === 'space') {
+				setCategoryList(res.spaceList.map((item: Space) => item.category))
+			}
+		})
+	}, [state.activeTab])
+	const saveHandler = (): void => {
+		if (state.activeTab === 'shortcut') {
+			const shortcut = {
+				id: state.id,
+				title: state.title,
+				url: state.url,
+				category: state.category,
+				icon: getIcon(state.url),
+			}
+			browser.storage.local.set({
+				shortcutList: [...shortcutList, shortcut],
+			})
+		} else if (state.activeTab === 'space') {
+			const space = {
+				id: state.id,
+				title: state.title,
+				url: state.url,
+				category: state.category,
+				icon: getIcon(state.url),
+				tags: state.spaceTags,
+			}
+			browser.storage.local.set({
+				spaceList: [...spaceList, space],
+			})
+		}
+		window.close()
+	}
 
-  useEffect(() => {
-    browser.storage.local.get().then((res) => {
-      if (state.activeTab === 'shortcut') {
-        setCategoryList(
-          res.shortcutList.map((item: Shortcut) => item.category)
-        );
-      } else if (state.activeTab === 'space') {
-        setCategoryList(res.spaceList.map((item: Space) => item.category));
-      }
-    });
-  }, [state.activeTab]);
-  const saveHandler = (): void => {
-    if (state.activeTab === 'shortcut') {
-      const shortcut = {
-        id: state.id,
-        title: state.title,
-        url: state.url,
-        category: state.category,
-        icon: getIcon(state.url),
-      };
-      browser.storage.local.set({
-        shortcutList: [...shortcutList, shortcut],
-      });
-    } else if (state.activeTab === 'space') {
-      const space = {
-        id: state.id,
-        title: state.title,
-        url: state.url,
-        category: state.category,
-        icon: getIcon(state.url),
-        tags: state.spaceTags,
-      };
-      browser.storage.local.set({
-        spaceList: [...spaceList, space],
-      });
-    }
-    window.close();
-  };
-
-  return (
-    <div className={styles.container}>
-      {!state.title ||
-        (!state.category && (
-          <span className={styles.errorMessage}>Fill the required fields</span>
-        ))}
-      <button
-        className={classnames(styles.button, styles.cancelButton)}
-        type='button'
-        onClick={(): void => {
-          // window.close();
-          browser.storage.local.clear();
-        }}
-      >
-        Cancel
-      </button>
-      <button
-        className={classnames(styles.button, styles.saveButton)}
-        type='button'
-        onClick={saveHandler}
-        disabled={
-          !state.category ||
-          !state.title ||
-          categoryList?.includes(state.category)
-        }
-      >
-        Save
-      </button>
-    </div>
-  );
-};
-export default PopupButtons;
+	return (
+		<div className={styles.container}>
+			{!state.title ||
+				(!state.category && (
+					<span className={styles.errorMessage}>Fill the required fields</span>
+				))}
+			<button
+				className={classnames(styles.button, styles.cancelButton)}
+				type='button'
+				onClick={(): void => {
+					window.close()
+					// browser.storage.local.clear()
+				}}
+			>
+				Cancel
+			</button>
+			<button
+				className={classnames(styles.button, styles.saveButton)}
+				type='button'
+				onClick={saveHandler}
+				disabled={state.isSubmitDisabled}
+			>
+				Save
+			</button>
+		</div>
+	)
+}
+export default PopupButtons
--- source/Popup/components/SpaceTags/SpaceTags.tsx
+++ source/Popup/components/SpaceTags/SpaceTags.tsx
@@ -1,52 +1,50 @@
-import React, {useContext, useEffect} from 'react';
-import {ContextProvider} from '../../context/context';
-import styles from './SpaceTags.module.scss';
+import React, { useContext, useEffect } from 'react'
+import { ContextProvider } from '../../context/context'
+import styles from './SpaceTags.module.scss'
 
 const SpaceTags: React.FC = () => {
-  const {dispatch} = useContext(ContextProvider);
-  const [tags, setTags] = React.useState<string[]>([]);
-  useEffect(() => {
-    dispatch({type: 'SET_SPACE_TAGS', payload: tags});
-  }, [tags, dispatch]);
-  const removeTagData = (indexToRemove: number): void => {
-    setTags([...tags.filter((_, index) => index !== indexToRemove)]);
-  };
-  const addTagData = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-    const element = event.target as HTMLInputElement;
-    if (element.value !== '') {
-      const string = element.value;
-      const stringToPush = string.slice(0, string.indexOf(','));
-      setTags([...tags, stringToPush]);
-      element.value = '';
-    }
-  };
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.h2}>Add Tags</h2>
-      <input
-        className={styles.input}
-        type='text'
-        placeholder='Add comma separated tags'
-        onKeyUp={(event): void =>
-          event.key === 'Enter' || event.which === 188
-            ? addTagData(event)
-            : undefined
-        }
-      />
-      <ul className={styles.tags}>
-        {tags.map((tag, index) => (
-          <li key={index} className={styles.tag}>
-            <span className={styles.tagTitle}>{tag}</span>
-            <span
-              className={styles.tagCloseIcon}
-              onClick={(): void => removeTagData(index)}
-            >
-              x
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-export default SpaceTags;
+	const { dispatch } = useContext(ContextProvider)
+	const [tags, setTags] = React.useState<string[]>([])
+	useEffect(() => {
+		dispatch({ type: 'SET_SPACE_TAGS', payload: tags })
+	}, [tags, dispatch])
+	const removeTagData = (indexToRemove: number): void => {
+		setTags([...tags.filter((_, index) => index !== indexToRemove)])
+	}
+	const addTagData = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+		const element = event.target as HTMLInputElement
+		if (element.value !== '') {
+			const string = element.value
+			const newTags = string.split(',')
+			setTags([...tags, ...newTags])
+			element.value = ''
+		}
+	}
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.h2}>Add Tags</h2>
+			<input
+				className={styles.input}
+				type='text'
+				placeholder='Add comma separated tags'
+				onKeyUp={(event): void =>
+					event.key === 'Enter' ? addTagData(event) : undefined
+				}
+			/>
+			<ul className={styles.tags}>
+				{tags.map((tag, index) => (
+					<li key={index} className={styles.tag}>
+						<span className={styles.tagTitle}>{tag}</span>
+						<span
+							className={styles.tagCloseIcon}
+							onClick={(): void => removeTagData(index)}
+						>
+							x
+						</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+export default SpaceTags
