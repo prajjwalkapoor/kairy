952f8d5ba2f3845ec9e7e0ece4db935fca530725
Vansh Mittal <103214265+mittalvansh@users.noreply.github.com>
2022-10-30 00:54:13 +0530

    Fixed : Issue #2 (#11)

--- source/utils/geticon.ts
+++ source/utils/geticon.ts
@@ -1,5 +1,12 @@
 export const getIcon = (url: string): string => {
-  const domain = url.split('//')[1].split('/')[0];
-  const icon = `https://www.google.com/s2/favicons?domain=${domain}`;
-  return icon;
-};
+  const domain = url.split('//')[1].split('/')[0]
+  const icon = `https://www.google.com/s2/favicons?domain=${domain}`
+  fetch(icon).then((res) => {
+    if (res.status === 404) {
+      return 'https://www.google.com/s2/favicons?domain=google.com'
+    } else {
+      return icon
+    }
+  })
+  return icon
+}
